c189be00abc311039b3724d52187795bbdf1fcfd
Hisashi Sawada <green.peace47@gmail.com>
2018-03-31 03:03:12 +0000

    Add bootstrap

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
-  
+
   def new
     if params[:back]
       @tweet = Tweet.new(tweet_params)
@@ -12,7 +12,7 @@ class TweetsController < ApplicationController
       @tweet = Tweet.new
     end
   end
-  
+
   def create
     @tweet = Tweet.new(tweet_params)
     if @tweet.save
@@ -27,12 +27,12 @@ class TweetsController < ApplicationController
 
   def edit
   end
-  
+
   def destroy
     @tweet.destroy
     redirect_to tweets_path, notice:"ツイートを削除しました！"
   end
-  
+
   def update
     if @tweet.update(tweet_params)
       redirect_to tweets_path, notice: "ツイートを編集しました！"
@@ -40,17 +40,17 @@ class TweetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def confirm
     @tweet = Tweet.new(tweet_params)
     render :new if @tweet.invalid?
   end
-  
+
   private
     def tweet_params
       params.require(:tweet).permit(:content)
     end
-  
+
     def set_tweet
       @tweet = Tweet.find(params[:id])
     end
