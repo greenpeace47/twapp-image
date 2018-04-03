be2573582eee68ee75c31b307dfecf771f2c7687
Hisashi Sawada <green.peace47@gmail.com>
2018-04-03 13:10:16 +0000

    upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      flash[:notice] = '登録しました。ログインしてください。'
+      redirect_to new_session_path
     else
       render 'new'
     end
